55e35f548c7982dc2451527e2d03672c4dab3b17
ye-gang-jjang <yegang13209@naver.com>
2023-09-04 13:54:54 +0900

    [1주차] Raf

--- Week 1/Raf/[백준 - 1213] 팰린드롬 만들기/[백준 - 1213] 팰린드롬 만들기.swift
+++ Week 1/Raf/[백준 - 1213] 팰린드롬 만들기/[백준 - 1213] 팰린드롬 만들기.swift
@@ -15,50 +15,29 @@ for char in input {
     alphabet[Int(char.asciiValue!)-65] += 1
 }
 
-if input.count % 2 == 0 {
-    var front = ""
-    var back = ""
-    
-    for i in 0..<26 {
-        if alphabet[i] % 2 != 0 {
+var front = ""
+var back = ""
+var mid = ""
+
+for i in 0..<26 {
+    if alphabet[i] % 2 != 0 {
+        if mid == "" {
+            mid += String(Character(UnicodeScalar(i+65)!))
+        } else {
             isPalindrome = false
             break
         }
-        
-        if alphabet[i] != 0 {
-            for _ in 0..<alphabet[i]/2 {
-                front += String(Character(UnicodeScalar(i+65)!))
-                back += String(Character(UnicodeScalar(i+65)!))
-            }
-        }
-    }
-    front += back.reversed()
-    print(isPalindrome == false ? "I'm Sorry Hansoo" : front)
-    
-} else {
-    var front = ""
-    var back = ""
-    var mid = ""
-    
-    for i in 0..<26 {
-        if alphabet[i] % 2 != 0 {
-            isPalindrome = false
-            break
-        }
-        
-        if alphabet[i] != 0 {
-            for _ in 0..<alphabet[i]/2 {
-                front += String(Character(UnicodeScalar(i+65)!))
-                back += String(Character(UnicodeScalar(i+65)!))
-            }
-            
-            if alphabet[i] % 2 != 0 {
-                mid += String(Character(UnicodeScalar(i+65)!))
-            }
-        }
     }
     
-    front += mid
-    front += back.reversed()
-    print(isPalindrome == false ? "I'm Sorry Hansoo" : front)
+    if alphabet[i] != 0 {
+        for _ in 0..<alphabet[i]/2 {
+            front += String(Character(UnicodeScalar(i+65)!))
+            back += String(Character(UnicodeScalar(i+65)!))
+        }
+    }
 }
+
+front += mid
+front += back.reversed()
+
+print(isPalindrome ? front : "I'm Sorry Hansoo")
